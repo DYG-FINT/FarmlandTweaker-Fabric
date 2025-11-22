7e57f24d847ee61b9a148033d23ece6a021f6159
DYG_FINT <102452600+DYG-FINT@users.noreply.github.com>
2025-11-23 03:51:47 +0800

    [FEAT](1.2) 添加踩踏调整、为配置项添加分类

--- src/main/java/io/dygfint/farmland_tweaker/config/ModConfig.java
+++ src/main/java/io/dygfint/farmland_tweaker/config/ModConfig.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static io.dygfint.farmland_tweaker.farmland_tweaker.isInitConfig;
 
-@Config(name = "irrigation_tweaker")
+@Config(name = "farmland_tweaker")
 public class ModConfig implements ConfigData {
     public static void init() {
         AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
@@ -26,16 +26,46 @@ public class ModConfig implements ConfigData {
         return AutoConfig.getConfigHolder(ModConfig.class).getConfig();
     }
 
+    @ConfigEntry.Category("irrigationTweaker")
+    @ConfigEntry.Gui.TransitiveObject
     @ConfigEntry.Gui.RequiresRestart
-    public boolean enable = true;
+    public IrrigationTweaker irrigationTweaker = new IrrigationTweaker();
+    @ConfigEntry.Category("trampleTweaker")
+    @ConfigEntry.Gui.TransitiveObject
+    @ConfigEntry.Gui.RequiresRestart
+    public TrampleTweaker trampleTweaker = new TrampleTweaker();
 
-    @ConfigEntry.Gui.RequiresRestart
-    public int rangeXZ = 4;
-    @ConfigEntry.Gui.RequiresRestart
-    public int rangeYmin = -1;
-    @ConfigEntry.Gui.RequiresRestart
-    public int rangeYmax = 0;
-    @ConfigEntry.Gui.RequiresRestart
-    @ConfigEntry.Gui.Tooltip(count = 2)
-    public List<String> extraHydrationBlocks = new ArrayList<>();
+    public static class IrrigationTweaker {
+        @ConfigEntry.Gui.RequiresRestart
+        public boolean enableIrrigationTweaker = true;
+        @ConfigEntry.Gui.RequiresRestart
+        public int rangeXZ = 4;
+        @ConfigEntry.Gui.RequiresRestart
+        public int rangeYmin = -1;
+        @ConfigEntry.Gui.RequiresRestart
+        public int rangeYmax = 0;
+        @ConfigEntry.Gui.Tooltip(count = 2)
+        @ConfigEntry.Gui.RequiresRestart
+        public List<String> extraHydrationBlocks = new ArrayList<>();
+    }
+
+    public static class TrampleTweaker {
+        @ConfigEntry.Gui.RequiresRestart
+        public boolean enableTrampleTweaker = true;
+        @ConfigEntry.Gui.Tooltip(count = 2)
+        @ConfigEntry.Gui.RequiresRestart
+        public double minTrampleFallHeight = 0.5;
+        @ConfigEntry.Gui.Tooltip(count = 2)
+        @ConfigEntry.Gui.RequiresRestart
+        public double trampleFallRange = 1.0;
+        @ConfigEntry.Gui.RequiresRestart
+        public boolean requireLivingEntityToTrample = true;
+        @ConfigEntry.Gui.RequiresRestart
+        public boolean allowPlayerTrample = true;
+        @ConfigEntry.Gui.Tooltip()
+        @ConfigEntry.Gui.RequiresRestart
+        public boolean allowMobTrample = true;
+        @ConfigEntry.Gui.RequiresRestart
+        public double trampleVolumeThreshold = 0.512;
+    }
 }
--- src/main/java/io/dygfint/farmland_tweaker/mixin/FarmlandBlockMixin.java
+++ src/main/java/io/dygfint/farmland_tweaker/mixin/FarmlandBlockMixin.java
@@ -1,17 +1,24 @@
 package io.dygfint.farmland_tweaker.mixin;
 
 import io.dygfint.farmland_tweaker.config.ModConfig;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FarmlandBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.tag.FluidTags;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Objects;
@@ -19,32 +26,43 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Mixin(FarmlandBlock.class)
-public class FarmlandBlockMixin {
-    @Unique private static final ModConfig config = ModConfig.get();
-    @Unique private static final int rangeXZ = config.rangeXZ;
-    @Unique private static final int rangeYmin = config.rangeYmin;
-    @Unique private static final int rangeYmax = config.rangeYmax;
+public abstract class FarmlandBlockMixin extends Block {
+    @Unique private static final ModConfig CONFIG = ModConfig.get();
+    @Unique private static final int RANGE_XZ = CONFIG.irrigationTweaker.rangeXZ;
+    @Unique private static final int RANGE_Y_MIN = CONFIG.irrigationTweaker.rangeYmin;
+    @Unique private static final int RANGE_Y_MAX = CONFIG.irrigationTweaker.rangeYmax;
     @Unique
-    private static final Set<Identifier> extraBlockIds = config.extraHydrationBlocks
+    private static final Set<Identifier> EXTRA_BLOCK_IDS = CONFIG.irrigationTweaker.extraHydrationBlocks
             .stream()
             .map(Identifier::tryParse)
             .filter(Objects::nonNull)
             .collect(Collectors.toSet());
 
+    @Unique private static final float MIN_FALL_DISTANCE = (float) CONFIG.trampleTweaker.minTrampleFallHeight;
+    @Unique private static final float CHANCE_RANGE = (float) CONFIG.trampleTweaker.trampleFallRange;
+    @Unique private static final boolean REQUIRE_LIVING_ENTITY_TO_TRAMPLE = CONFIG.trampleTweaker.requireLivingEntityToTrample;
+    @Unique private static final boolean ALLOW_PLAYER_TRAMPLE = CONFIG.trampleTweaker.allowPlayerTrample;
+    @Unique private static final boolean ALLOW_MOB_TRAMPLE = CONFIG.trampleTweaker.allowMobTrample;
+    @Unique private static final float TRAMPLE_VOLUME_THRESHOLD = (float) CONFIG.trampleTweaker.trampleVolumeThreshold;
+
+    public FarmlandBlockMixin(Settings settings) {
+        super(settings);
+    }
+
     @Inject(method = "isWaterNearby", at = @At("HEAD"), cancellable = true)
-    private static void farmland_tweaker$onIsNearWater(WorldView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-        if (!config.enable) return;
+    private static void farmland_tweaker$modifyIsWaterNearby(WorldView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        if (!CONFIG.irrigationTweaker.enableIrrigationTweaker) return;
 
         BlockPos.Mutable m = new BlockPos.Mutable();
 
-        for (int dy = rangeYmin; dy <= rangeYmax; dy++) {
-            for (int dx = -rangeXZ; dx <= rangeXZ; dx++) {
-                for (int dz = -rangeXZ; dz <= rangeXZ; dz++) {
+        for (int dy = RANGE_Y_MIN; dy <= RANGE_Y_MAX; dy++) {
+            for (int dx = -RANGE_XZ; dx <= RANGE_XZ; dx++) {
+                for (int dz = -RANGE_XZ; dz <= RANGE_XZ; dz++) {
                     m.set(pos.getX() + dx, pos.getY() + dy, pos.getZ() + dz);
                     BlockState state = world.getBlockState(m);
                     Identifier id = Registries.BLOCK.getId(state.getBlock());
 
-                    if (world.getFluidState(m).isIn(FluidTags.WATER) || extraBlockIds.contains(id)) {
+                    if (world.getFluidState(m).isIn(FluidTags.WATER) || EXTRA_BLOCK_IDS.contains(id)) {
                         cir.setReturnValue(true);
                         return;
                     }
@@ -53,5 +71,27 @@ public class FarmlandBlockMixin {
         }
 
         cir.setReturnValue(false);
+    }
+
+    @Inject(method = "onLandedUpon", at = @At("HEAD"), cancellable = true)
+    private void farmland_tweaker$modifyLandedUpon(World world, BlockState state, BlockPos pos, Entity entity, double fallDistance, CallbackInfo ci) {
+        if (!CONFIG.trampleTweaker.enableTrampleTweaker) return;
+
+        if (world instanceof ServerWorld) {
+            float chanceValue = (float) ((fallDistance - MIN_FALL_DISTANCE) / CHANCE_RANGE);
+            float entityVolume = entity.getWidth() * entity.getWidth() * entity.getHeight();
+
+            boolean passedTrampleChance = world.random.nextFloat() < chanceValue;
+            boolean isLivingEntityRequirementMet = REQUIRE_LIVING_ENTITY_TO_TRAMPLE ? entity instanceof LivingEntity : true;
+            boolean isTrampleAllowed = entity instanceof PlayerEntity ? ALLOW_PLAYER_TRAMPLE : ALLOW_MOB_TRAMPLE;
+            boolean isLargeEnoughToTrample  = entityVolume > TRAMPLE_VOLUME_THRESHOLD;
+
+            if (passedTrampleChance && isLivingEntityRequirementMet && isTrampleAllowed && isLargeEnoughToTrample) {
+                FarmlandBlock.setToDirt(entity, state, world, pos);
+            }
+        }
+
+        super.onLandedUpon(world, state, pos, entity, fallDistance);
+        ci.cancel();
     }
 }
