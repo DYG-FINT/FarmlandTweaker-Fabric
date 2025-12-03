72504ad790682ae8b5667cfa8e3a56717260ea5c
DYG_FINT <102452600+DYG-FINT@users.noreply.github.com>
2025-12-03 21:45:43 +0800

    [REF] 将Redirect改为ModifyExpressionValue

--- src/main/java/me/dygfint/farmland_tweaker/mixin/MoistureTweakerMixin.java
+++ src/main/java/me/dygfint/farmland_tweaker/mixin/MoistureTweakerMixin.java
@@ -1,29 +1,21 @@
 package me.dygfint.farmland_tweaker.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import me.dygfint.farmland_tweaker.config.ModConfig;
 import net.minecraft.block.FarmlandBlock;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(FarmlandBlock.class)
 public abstract class MoistureTweakerMixin {
-    @Shadow
-    private static boolean hasCrop(BlockView world, BlockPos pos) {
-        throw new AssertionError();
-    }
-
-    @Redirect(
+    @ModifyExpressionValue(
             method = "randomTick",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/block/FarmlandBlock;hasCrop(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)Z"
             )
     )
-    private boolean farmland_tweaker$redirectHasCrop(BlockView world, BlockPos pos) {
+    private boolean farmland_tweaker$redirectHasCrop(boolean original) {
         ModConfig.MoistureTweaker config = ModConfig.get().moistureTweaker;
         if (config.enableMoistureTweaker) {
             if (!config.preventCropFarmlandDryToDirt) {
@@ -31,6 +23,6 @@ public abstract class MoistureTweakerMixin {
             }
         }
 
-        return hasCrop(world, pos);
+        return original;
     }
 }
